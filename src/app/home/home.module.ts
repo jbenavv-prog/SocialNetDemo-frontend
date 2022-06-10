929dd80cb5f9771c637b9332a21abafded84be21
Julian Camilo Benavides Vanegas <julian.benavides@comware.com.co>
2022-06-10 17:24:04 -0500

    Publish Preview Finished

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -21,6 +22,7 @@ import { MatInputModule } from '@angular/material/input';
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
+    MatIconModule,
   ]
 })
 export class HomeModule { }
