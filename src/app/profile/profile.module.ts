d5c1510c52ddf2d5291a8d526e01715ef21caaea
Julian Camilo Benavides Vanegas <julian.benavides@comware.com.co>
2022-06-12 11:07:38 -0500

    profiles completed

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
+import {MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 
 
@@ -9,7 +15,14 @@ import { ProfileComponent } from './profile/profile.component';
     ProfileComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ]
 })
 export class ProfileModule { }
--- src/app/profile/profile/profile.component.ts
+++ src/app/profile/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AuthService, ProfileService, PublicationService } from 'src/app/_services';
+import { AuthService, CommentService, ProfileService, PublicationService, ReactionService } from 'src/app/_services';
 
 @Component({
   selector: 'app-profile',
@@ -14,16 +14,20 @@ export class ProfileComponent implements OnInit {
     private authService: AuthService,
     private profileService: ProfileService,
     private publicationService: PublicationService,
+    private commentService: CommentService,
+    private reactionService: ReactionService,
   ) {
     this.user = this.authService.userValue;
   }
 
   profileId: any;
   profile: any;
-  publication: any;
+  publications: any;
   user: any;
   defaultBanner: string = '../../../assets/images/avatar/defaultBanner.jpg'
   defaultImgAvatar: string = '../../../assets/images/avatar/defaultAvatar.png';
+  likes: any = {};
+  commentsEnabled: boolean = false;
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(paramMap => {
@@ -42,10 +46,76 @@ export class ProfileComponent implements OnInit {
 
     this.publicationService.getPublicationsByUser(user).subscribe(response => {
       console.log(response);
-      const resp = response;
-      this.publication = resp;
+      const resp: any = response;
+      this.publications = resp.data.result;
     })
-
   }
 
+  like(event: any) {
+    let idPublication: number;
+
+    if (event.target.id) {
+      idPublication = event.target.id
+    } else {
+      idPublication = event.target.parentElement.id
+    }
+
+    let like: any;
+
+    if (this.likes[idPublication]) {
+      like = this.likes[idPublication].like;
+      if (this.likes[idPublication].like == 1) {
+        like = 0;
+      } else {
+        like = 1;
+      }
+    }
+
+    const idTypeReaction = 1;
+
+    Object.assign(this.likes, { [idPublication]: { like, idTypeReaction } });
+
+    const reaction = {
+      idPublication,
+      like,
+      idTypeReaction,
+    }
+
+    const request = {
+      user: this.user.data,
+      reaction
+    }
+
+    this.reactionService.create(request).subscribe(response => {
+      this.ngOnInit();
+    })
+  }
+
+  validateLike(idAccountsWhoLiked: any, idPublication: any) {
+    for (let i in idAccountsWhoLiked) {
+      if (idAccountsWhoLiked[i] == this.user.data.id) {
+        Object.assign(this.likes, { [idPublication]: { like: 1, idTypeReaction: 1 } });
+        return true;
+      }
+    }
+    Object.assign(this.likes, { [idPublication]: { like: 0, idTypeReaction: 1 } });
+    return false;
+  }
+
+  onEnterComment(event: any) {
+    const comment = {
+      idPublication: event.target.id,
+      description: event.target.value
+    }
+
+    const request = {
+      user: this.user.data,
+      comment
+    }
+
+    this.commentService.create(request).subscribe(response => {
+      console.log(response);
+      this.ngOnInit();
+    })
+  }
 }
